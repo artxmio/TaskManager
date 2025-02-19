9c06ef7595e459dd4ff6abebf2e308ab7cc60313
artxmix <artem.artusevskij01@gmail.com>
2025-02-19 10:30:11 +0300

    MainWindow TabControl

--- Model/ProjectModel/Project.cs
+++ Model/ProjectModel/Project.cs
@@ -5,8 +5,8 @@ namespace TaskManager.Model.ProjectModel;
 public class Project
 {
     public int ProjectId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
     public int CountParticipants { get; set; }
     public bool IsComplete { get; set; }
 }
